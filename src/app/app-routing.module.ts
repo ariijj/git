c8727c6ffc9c64ea3616c5f9098a0e5141376173
ariijj <belaidarij3@gmail.com>
2023-08-08 08:46:03 +0100

    frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
+import { StudentComponent } from './student/student.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { DoSectionComponent } from './section/do-section.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'section',component:DoSectionComponent},
+  {path:'accueil',component:AccueilComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'portfolio',component:PortfolioComponent},
+  {path:'services',component:ServicesComponent},
+  {path:'student',component:StudentComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { Router } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { DoSectionComponent } from './section/do-section.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+
 
 @NgModule({
+  
+ 
+ 
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    HomeComponent,
+    InfoComponent,
+    AccueilComponent,
+    DoSectionComponent,
+    SignupComponent,
+    LoginComponent
+    
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private router : Router){}
+}
